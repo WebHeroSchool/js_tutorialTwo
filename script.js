14042b52724f7d7cc70389370cdf2dbd1531bfa8
Nadir-bnm <nadir.begiyev@gmail.com>
2021-03-24 18:17:36 +0800

    Yo

---
+++ script.js
@@ -0,0 +1,23 @@
+let questionOne = {
+  question: 'what?',
+  answer: 'example',
+  correctAnswer: 'example'
+}
+
+let questionTwo = {
+  question: 'when?',
+  answer: 'example',
+  correctAnswer: 'example'
+}
+
+let questionThree = {
+  question: 'where?',
+  answer: 'example',
+  correctAnswer: 'example'
+}
+
+let questionFour = {
+  question: 'why?',
+  answer: 'example',
+  correctAnswer: 'example'
+}
